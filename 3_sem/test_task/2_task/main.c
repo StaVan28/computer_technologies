03eb7faca4c282afd61be321e66d6455d371d2c4
Starchenko Ivan <starchenko.ia@phystech.edu>
2021-10-02 01:52:52 +0300

    add dir:test_tasks

---
+++ 3_sem/test_task/2_task/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>		
+#include <fcntl.h>
+#include <errno.h>
+
+//-------------------------------------------------------------
+
+const int    STDIN_ARGS = 3;
+const mode_t  MODE_OPEN = S_IRUSR | S_IWUSR	;
+
+//!
+
+#define BUFF_SIZE 4096
+
+//-------------------------------------------------------------
+
+void my_cp(const char* out_file_path, const char* in_file_path);
+
+//-------------------------------------------------------------
+
+int main(int argc, const char *argv[])
+{
+	if (argc != STDIN_ARGS)
+	{
+		fprintf(stderr, "ERROR! \nExecute: ./a_out <out_file> <in_file>\n");
+		return EXIT_FAILURE;
+	}
+
+	my_cp(argv[1], argv[2]);
+
+	return 0;
+}
+
+//-------------------------------------------------------------
+
+void my_cp(const char* out_file_path, const char* in_file_path)
+{
+	int  num_symb        = 0;
+	char buff[BUFF_SIZE] = {};
+
+	errno = 0;
+	int out_fd = open(out_file_path, O_RDONLY);
+	if (errno != 0)
+	{
+		fprintf(stderr, "ERROR! open() out_file\n");
+		exit(EXIT_FAILURE);		
+	}
+
+	errno = 0;
+	int  in_fd = open( in_file_path, O_WRONLY | O_CREAT, MODE_OPEN);
+	if (errno != 0)
+	{
+		fprintf(stderr, "ERROR! open() in_file\n");
+		exit(EXIT_FAILURE);		
+	}
+
+	while ((num_symb = read(out_fd, buff, BUFF_SIZE)) > 0)
+	{
+		if (write(in_fd, buff, num_symb) != num_symb)
+		{
+			fprintf(stderr, "ERROR! Something wrong with write()\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (num_symb < 0)
+	{
+		fprintf(stderr, "ERROR! Something wrong with read()\n");
+		exit(EXIT_FAILURE);		
+	}	
+}
+
+//-------------------------------------------------------------
---
+++ 3_sem/test_task/6_task/main.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+//-------------------------------------------------------------
+
+const int STDIN_ARGS = 2; 
+
+//
+
+#define BUFF_SIZE 4096
+
+//-------------------------------------------------------------
+
+int main(const int argc, char const *argv[])
+{
+	if (argc != STDIN_ARGS)
+	{
+		fprintf(stderr, "ERROR! \nExecute: ./a_out <file> \n");
+		return EXIT_FAILURE;
+	}
+ 
+	pid_t pid;
+	int   fd[2]           = {};
+	char  buff[BUFF_SIZE] = {};
+	int   num_symb        = 0;
+
+	if (pipe(fd) < 0)
+	{
+		fprintf(stderr, "ERROR! Smth wrong with pipe()\n");
+		exit(EXIT_FAILURE);
+	}		
+
+	if ((pid = fork()) < 0)
+	{
+		fprintf(stderr, "ERROR! Smth wrong with fork()\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+	{
+		waitpid(pid, NULL, 0);
+
+		close(fd[1]);
+
+		while ((num_symb = read(fd[0], buff, BUFF_SIZE)) > 0)
+		{
+			if (write(STDOUT_FILENO, buff, num_symb) != num_symb)
+			{
+				fprintf(stderr, "ERROR! Something wrong with write()\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (num_symb < 0)
+		{
+			fprintf(stderr, "ERROR! Something wrong with read()\n");
+			exit(EXIT_FAILURE);		
+		}		
+
+		exit(0);
+	}
+	else
+	{
+		close(fd[0]);
+
+		errno = 0;
+		int out_file = open(argv[1], O_RDONLY);
+		if (errno != 0)
+		{
+			fprintf(stderr, "ERROR! open() out_file\n");
+			exit(EXIT_FAILURE);		
+		}
+
+		while ((num_symb = read(out_file, buff, BUFF_SIZE)) > 0)
+		{
+			if (write(fd[1], buff, num_symb) != num_symb)
+			{
+				fprintf(stderr, "ERROR! Something wrong with write()\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (num_symb < 0)
+		{
+			fprintf(stderr, "ERROR! Something wrong with read()\n");
+			exit(EXIT_FAILURE);		
+		}
+
+		exit(0);
+	}
+	
+	exit(0);
+}
+
+//-------------------------------------------------------------
