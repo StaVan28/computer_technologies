89f089f74a30a7d866ecc184bd90b5e58077cf23
Starchenko Ivan <starchenko.ia@phystech.edu>
2021-11-26 19:09:23 +0300

    (Sharmrm) SEM_UNDO to full and empty

--- 3_sem/Sharmem/sharmem_config.h
+++ 3_sem/Sharmem/sharmem_config.h
@@ -39,17 +39,17 @@ enum SEM
 };
 
 //! send/get data
-static struct sembuf p_empty[1] = {EMPTY, -1, SEM_UNDO};
-static struct sembuf v_empty[1] = {EMPTY,  1, SEM_UNDO};
-static struct sembuf p_full [1] = {FULL , -1, SEM_UNDO};
-static struct sembuf v_full [1] = {FULL,   1, SEM_UNDO};
+static struct sembuf p_empty[1] = {EMPTY, -1, 0};
+static struct sembuf v_empty[1] = {EMPTY,  1, 0};
+static struct sembuf p_full [1] = {FULL , -1, 0};
+static struct sembuf v_full [1] = {FULL,   1, 0};
 static struct sembuf p_mutex[1] = {MUTEX, -1, SEM_UNDO};
 static struct sembuf v_mutex[1] = {MUTEX,  1, SEM_UNDO};
 
 //!
 
 static const int    DFLT_FTOK_SEED = 28;
-static const char*  DFLT_FTOK_PATH = "../test.txt";
+static const char*  DFLT_FTOK_PATH = "/bin/bash";
 static const mode_t DFLT_MODE      = 0660;
 
 #define PAGE_SIZE 4096
